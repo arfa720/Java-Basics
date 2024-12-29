01e6f20814690ca0561d317b335649de79ea6651
Muhammad Arfa <129337578+arfa720@users.noreply.github.com>
2024-12-29 23:49:33 +0500

    Add files via upload

---
+++ 05_Loops/Loops.java
@@ -0,0 +1,16 @@
+// 05_Loops.java
+public class Loops {
+    public static void main(String[] args) {
+        // For loop
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
+
+        // While loop
+        int count = 1;
+        while (count <= 5) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+    }
+}
