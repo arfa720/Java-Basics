14165cb159841e303093f4b218e88d03506c63cd
Muhammad Arfa <129337578+arfa720@users.noreply.github.com>
2024-12-29 23:49:52 +0500

    Add files via upload

---
+++ 06_Functions/Functions.java
@@ -0,0 +1,12 @@
+// 06_Functions.java
+public class Functions {
+    // Function definition
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = addNumbers(10, 20); // Function call
+        System.out.println("Sum: " + result);
+    }
+}
