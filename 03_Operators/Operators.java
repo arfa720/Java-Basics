ce6d72f27dd8de8e2fcc688867e79c337b81eb7f
Muhammad Arfa <129337578+arfa720@users.noreply.github.com>
2024-12-29 23:46:59 +0500

    Add files via upload

---
+++ 03_Operators/Operators.java
@@ -0,0 +1,20 @@
+// 03_Operators.java
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        // Arithmetic Operators
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Relational Operators
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a == b: " + (a == b));
+
+        // Logical Operators
+        System.out.println("true AND false: " + (true && false));
+    }
+}
